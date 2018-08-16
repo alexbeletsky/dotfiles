83214d2d67777269c583499a6edfe905ad563980
Alexander Beletsky <alexander.beletsky@gmail.com>
2018-08-16 11:22:27 +0200

    latest changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,23 @@
 alias reload!='. ~/.zshrc'
 alias rake='noglob rake'
+
 alias npmw='npm run-script watch'
 alias npmt='npm test'
 alias npmtw='npm run-script test-watch'
 alias npms='npm start'
+
 alias mongodfts='mongod --setParameter textSearchEnabled=true'
 alias mongodd='mongod --config /usr/local/etc/mongod.conf'
+
+alias docker-kill-all='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+# ls, the common ones I use a lot shortened for rapid fire usage
+alias l='ls -lFh'     #size,show type,human readable
+alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -l'      #long list
+alias ldot='ls -ld .*'
+alias lS='ls -1FSsh'
+alias lart='ls -1Fcart'
+alias lrt='ls -1Fcrt'
